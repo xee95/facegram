84dbbc780aa268e53d4c0d9f2b2fdfff91763e39
xee95 <zeeshannawaz2017@gmail.com>
2023-11-09 12:44:05 +0500

    singup form ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { AuthLayout } from './_auth/AuthLayout';
+import  AuthLayout  from './_auth/AuthLayout';
 import { SigninForm } from './_auth/forms/SigninForm';
-import { SignupForm } from './_auth/forms/SignupForm';
+import  SignupForm  from './_auth/forms/SignupForm';
 import { RootLayout } from './_root/RootLayout';
 import { Home } from './_root/pages';
 import './global.css'
